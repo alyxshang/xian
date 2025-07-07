7b796d9c16127186dd2902ff4513fcd5c94feb00
alyxshang <179976644+alyxshang@users.noreply.github.com>
2025-07-07 18:42:31 +0200

    Fixed the doc generation.

--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,24 @@
 // Xian by Alyx Shang.
 // Licensed under the FSL v1.
 
+// Declaring the error module.
+const err = @import("err.zig");
+
+// Declaring the main "string"
+// module.
+const string = @import("string.zig");
+
+// Declaring the "slices" module.
+const slices = @import("slices.zig");
+
 // Exporting the module for handling 
 // errors in this library.
-pub usingnamespace @import("err.zig");
+pub usingnamespace err;
 
 // Exporting the main module providing
 // the `String` structure.
-pub usingnamespace @import("string.zig");
+pub usingnamespace string;
 
 // Exporting the module providing
 // functions for string slices.
-pub usingnamespace @import("slices.zig");
+pub usingnamespace slices;

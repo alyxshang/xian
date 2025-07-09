1f79e2d68318024c039233fddd3ebc83c303ca61
🖤 𝓐𝓛𝓨𝓧 - 天权🖤🐉 <179976644+alyxshang@users.noreply.github.com>
2025-07-09 13:37:02 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -10,24 +10,25 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const lib_mod = b.createModule(
-        .{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = optimize,
-        }
-    );
     _ = b.addModule(
         "xian",
         .{
-            .root_source_file = b.path("src/root.zig")
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize
         }
     );
     const lib = b.addLibrary(
         .{
             .linkage = .static,
             .name = "xian",
-            .root_module = lib_mod,
+            .root_module = b.createModule(
+                .{
+                    .root_source_file = b.path("src/root.zig"),
+                    .target = target,
+                    .optimize = optimize,
+                }
+            ),
         }
     );
     b.installArtifact(lib);
